e3ad6d962045916af109928e524598343dfab98d
ElisavetAmp <elisabet.ampatzidou@gmail.com>
2022-12-02 00:31:46 +0200

    fix: change path to assets file

--- clip/initParams.js
+++ clip/initParams.js
@@ -6,21 +6,21 @@ export default [
       dishes:[
         {
           name:"Pizza Special",
-          imageUrl:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-special.png",
+          image:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-special.png",
           description:"With tomato sauce, gouda, mushrooms, pepper & bacon",
           price:"8.30€",
           offer:"-10%"
         },
         {
           name:"Pizza Margarita",
-          imageUrl:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-margarita.png",
+          image:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-margarita.png",
           description:"With tomato sauce & gouda",
           price:"7.40€",
           offer:"1&1"
         },
         {
           name:"Pizza Pepperoni",
-          imageUrl:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-special.png",
+          image:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-special.png",
           description:"With tomato sauce, gouda, pepperoni sausage",
           price:"7.80€",
           offer:"-1€"
