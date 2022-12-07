15dd180ac00eeb21ba8f59874cdb6392790a23c7
ivanvl01 <1v4nvl@gmail.com>
2022-12-07 19:52:06 +0200

    fix: fix size and space

--- clip/initParams.js
+++ clip/initParams.js
@@ -1,35 +1,38 @@
 export default [
   {
     value: {
-      name: "John",
+      name: "Theofilos Papadopoulos",
       category: "Italian",
-      city:"Thessaloniki",
-      dishes:[
+      city: "Thessaloniki",
+      dishes: [
         {
-          name:"Pizza Special",
-          image:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-special.png",
-          description:"With tomato sauce, gouda, mushrooms, pepper & bacon",
-          price:"8.30",
-          offer:"7.10",
-          brand:"Pizza Fan",
+          name: "Pizza Special",
+          image:
+            "https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-special.png",
+          description: "With tomato sauce, gouda, mushrooms, pepper & bacon",
+          price: "8.30",
+          offer: "7.10",
+          brand: "Pizza Fan",
         },
         {
-          name:"Pizza Margarita",
-          image:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-margarita.png",
-          description:"With tomato sauce & gouda",
-          price:"7.40",
-          offer:"5.90",
-          brand:"Pizza Spot",
+          name: "Pizza Margarita",
+          image:
+            "https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-margarita.png",
+          description: "With tomato sauce & gouda",
+          price: "7.40",
+          offer: "5.90",
+          brand: "Pizza Spot",
         },
         {
-          name:"Pizza Pepperoni",
-          image:"https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-pepperoni.png",
-          description:"With tomato sauce, gouda, pepperoni sausage",
-          price:"7.80",
-          offer:"6.60",
-          brand:"Roma Pizza",
-        }
-      ]
+          name: "Pizza Pepperoni",
+          image:
+            "https://elisavetampatzidou.github.io/food-promo-clip/server/assets/pizza-pepperoni.png",
+          description: "With tomato sauce, gouda, pepperoni sausage",
+          price: "7.80",
+          offer: "6.60",
+          brand: "Roma Pizza",
+        },
+      ],
     },
   },
 ];
