4b135fa884f386b932a6974b43248024fe14a23e
ElisavetAmp <elisabet.ampatzidou@gmail.com>
2022-12-07 18:19:27 +0200

    extra changes

--- clip/clip.js
+++ clip/clip.js
@@ -35,11 +35,10 @@ export const clip = new HTMLClip({
 
 clip.addIncident(hexagonCircle(800,450,50,".container1"),0);
 
-clip.addIncident(textReveal('IBM Plex Sans Arabic, sans-serif',"We have some",".quote1",1500),500);
-clip.addIncident(textReveal('IBM Plex Sans Arabic, sans-serif',"deals for you",".quote2",1500),500);
-clip.addIncident(textReveal('IBM Plex Sans Arabic, sans-serif',"who love",".quote3a",1500),500);
-clip.addIncident(textReveal('IBM Plex Sans Arabic, sans-serif',"@initParams.category",".quote3b",1500),500);
-clip.addIncident(textReveal('IBM Plex Sans Arabic, sans-serif',"food, near you!",".quote4",1500),500);
+clip.addIncident(textReveal('Palatino, serif',"For you that love",".quote1a",1500),500);
+clip.addIncident(textReveal('Palatino, serif',"@initParams.category",".quote1b",1500),500);
+clip.addIncident(textReveal('Palatino, serif',"we have the best deals",".quote2",1500),500);
+clip.addIncident(textReveal('Palatino, serif',"near you",".quote3",1500),500);
 
 clip.addIncident(opacity(1,".l",300,"@stagger(0,500)"),200);
 clip.addIncident(topMove("100%",".container1",500),4000);
@@ -53,10 +52,12 @@ const startTime3 = startTime+startTime2+3500;
 clip.addIncident(scale(2,".dish:nth-child(1) .image",50,0,"easeOutSine"),startTime);
 clip.addIncident(hexagonCircle(800,450,50,".dish:nth-child(1) .background1"),startTime);
 clip.addIncident(ripples(".dish:nth-child(1) .background2", 7000),startTime);
-clip.addIncident(leftMove("55%",".dish:nth-child(1) .info",800,0,"easeOutBounce"),200+startTime);
+clip.addIncident(leftMove("40%",".dish:nth-child(1) .info",800,0,"easeOutBounce"),200+startTime);
 clip.addIncident(leftMove("0%",".dish:nth-child(1) .description",500,0,"easeOutSine"),1000+startTime);
 clip.addIncident(rotatedLineReveal("@initParams.dishes[0].brand","@initParams.city",".restaurant"),startTime+1000);
-clip.addIncident(svgDraw(1,"svg path",2000,"@stagger(0,300)"),3000+startTime);
+clip.addIncident(topMove("0%",".stars",400),startTime+3200);
+clip.addIncident(topMove("-100%",".stars",400),startTime+6000);
+clip.addIncident(svgDraw(1,".circles-svg path",2000,"@stagger(0,300)"),3000+startTime);
 clip.addIncident(scale(1,".dish:nth-child(1) .price-wrapper",800),3500+startTime);
 clip.addIncident(line(".dish:nth-child(1) .price",200),5200+startTime);
 clip.addIncident(opacity(0,".dish:nth-child(1) .offer",200),5500+startTime);
@@ -67,7 +68,7 @@ clip.addIncident(rotate("0deg",".dish:nth-child(1) .image",3500),3550+startTime)
 //circle
 clip.addIncident(scale(3,".top-circle",500),7000+startTime);
 clip.addIncident(opacity(0,".dish:nth-child(1)",1),7000+startTime+490);
-clip.addIncident(svgDraw(0,"svg path",1),7000+startTime+490);
+clip.addIncident(svgDraw(0,".circles-svg path",1),7000+startTime+490);
 clip.addIncident(opacity(1,".dish:nth-child(2)",1),7000+startTime+490);
 clip.addIncident(opacity(0,".top-circle",500),7000+startTime+500);
 clip.addIncident(scale(0,".top-circle",1),7000+startTime+1000);
@@ -76,10 +77,12 @@ clip.addIncident(opacity(1,".top-circle",1),7000+startTime+1000);
 clip.addIncident(scale(2,".dish:nth-child(2) .image",50,0,"easeOutSine"),startTime2);
 clip.addIncident(hexagonCircle(800,450,50,".dish:nth-child(2) .background1"),startTime2);
 clip.addIncident(ripples(".dish:nth-child(2) .background2", 7000),startTime2);
-clip.addIncident(leftMove("55%",".dish:nth-child(2) .info",800,0,"easeOutBounce"),200+startTime2);
+clip.addIncident(leftMove("40%",".dish:nth-child(2) .info",800,0,"easeOutBounce"),200+startTime2);
 clip.addIncident(leftMove("0%",".dish:nth-child(2) .description",500,0,"easeOutSine"),1000+startTime2);
 clip.addIncident(rotatedLineReveal("@initParams.dishes[1].brand","@initParams.city",".restaurant"),startTime2+1000);
-clip.addIncident(svgDraw(1,"svg path",2000,"@stagger(0,300)"),3000+startTime2);
+clip.addIncident(topMove("0%",".stars",400),startTime2+3200);
+clip.addIncident(topMove("-100%",".stars",400),startTime2+6000);
+clip.addIncident(svgDraw(1,".circles-svg path",2000,"@stagger(0,300)"),3000+startTime2);
 clip.addIncident(scale(1,".dish:nth-child(2) .price-wrapper",800),3500+startTime2);
 clip.addIncident(line(".dish:nth-child(2) .price",200),5200+startTime2);
 clip.addIncident(opacity(0,".dish:nth-child(2) .offer",200),5500+startTime2);
@@ -90,7 +93,7 @@ clip.addIncident(rotate("0deg",".dish:nth-child(2) .image",3500),3550+startTime2
 //circle
 clip.addIncident(scale(3,".top-circle",500),7000+startTime2);
 clip.addIncident(opacity(0,".dish:nth-child(2)",1),7000+startTime2+490);
-clip.addIncident(svgDraw(0,"svg path",1),7000+startTime2+490);
+clip.addIncident(svgDraw(0,".circles-svg path",1),7000+startTime2+490);
 clip.addIncident(opacity(1,".dish:nth-child(3)",1),7000+startTime2+490);
 clip.addIncident(opacity(0,".top-circle",500),7000+startTime2+500);
 clip.addIncident(scale(0,".top-circle",1),7000+startTime2+1000);
@@ -99,10 +102,12 @@ clip.addIncident(opacity(1,".top-circle",1),7000+startTime2+1000);
 clip.addIncident(scale(2,".dish:nth-child(3) .image",50,0,"easeOutSine"),startTime3);
 clip.addIncident(hexagonCircle(800,450,50,".dish:nth-child(3) .background1"),startTime3);
 clip.addIncident(ripples(".dish:nth-child(3) .background2", 7000),startTime3);
-clip.addIncident(leftMove("55%",".dish:nth-child(3) .info",800,0,"easeOutBounce"),200+startTime3);
+clip.addIncident(leftMove("40%",".dish:nth-child(3) .info",800,0,"easeOutBounce"),200+startTime3);
 clip.addIncident(leftMove("0%",".dish:nth-child(3) .description",500,0,"easeOutSine"),1000+startTime3);
 clip.addIncident(rotatedLineReveal("@initParams.dishes[2].brand","@initParams.city",".restaurant"),startTime3+1000);
-clip.addIncident(svgDraw(1,"svg path",2000,"@stagger(0,300)"),3000+startTime3);
+clip.addIncident(topMove("0%",".stars",400),startTime3+3200);
+clip.addIncident(topMove("-100%",".stars",400),startTime3+6000);
+clip.addIncident(svgDraw(1,".circles-svg path",2000,"@stagger(0,300)"),3000+startTime3);
 clip.addIncident(scale(1,".dish:nth-child(3) .price-wrapper",800),3500+startTime3);
 clip.addIncident(line(".dish:nth-child(3) .price",200),5200+startTime3);
 clip.addIncident(opacity(0,".dish:nth-child(3) .offer",200),5500+startTime3);
@@ -113,7 +118,7 @@ clip.addIncident(rotate("0deg",".dish:nth-child(3) .image",3500),3550+startTime3
 //circle
 clip.addIncident(scale(3,".top-circle",500),7000+startTime3);
 clip.addIncident(opacity(0,".dish:nth-child(3)",1),7000+startTime3+490);
-clip.addIncident(opacity(0,"svg",1),7000+startTime3+490);
+clip.addIncident(opacity(0,".circles-svg",1),7000+startTime3+490);
 clip.addIncident(opacity(0,".top-circle",500),7000+startTime3+500);
 clip.addIncident(opacity(0,".container2",500),7000+startTime3+500);
 clip.addIncident(topMove("100%",".container2",1),7000+startTime3+500+500);
